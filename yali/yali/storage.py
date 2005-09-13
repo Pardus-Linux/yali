1655b70e70266089e2cf162f0e3cb9cf1d86cf98
Baris Metin <bmetin@yelp.com>
2005-09-13 12:26:51 +0000

    Self-explanatory three modules will be used in YALI. No code yet ;)
    -----
    YALI içerisinde kullanılacak endini anlatan üç modül. Henüz kod yok ;)
    (Bundan sonra commentleri İngilizce yazmaya çalışacağım. Sanırım
    dışarıdan gelmesi muhtemel destek için önemli. Zaten kod içerisindeki
    yorumlar İngilizce...)

---
+++ yali/yali/storage.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+# Storage module will handle the disk/partitioning basics for
+# installation. Basically this module can provide two main interfaces,
+# one for physical storage and one for partitions.
---
+++ yali/yali/systemcfg.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+# SystemConfiguration will be a single storage to collect
+# configuration data created/used in installation. 
+# This may also be used for automatic installations providing an
+# installprofile.cfg (just an idea for now ;).
