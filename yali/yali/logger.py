78c20947a11a43ce7eaf6c42e9d4336c1274b9c5
Baris Metin <bmetin@yelp.com>
2005-09-13 12:46:30 +0000

    - basic Steps implementation - logger template

---
+++ yali/yali/logger.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+# Logger module which can log/write various information to different
+# places (log file, tty...)
--- yali/yali/steps.py
+++ yali/yali/steps.py
@@ -8,3 +8,43 @@
 # 'basic setup'. Each stage can have different number of screens
 # though...
 
+
+class Steps:
+
+    def __init__(self):
+        self._steps = {}
+        self._current = None
+
+    ##
+    # Add a new step.
+    # @param index(int): index number used for the ordering of steps.
+    # @param step(ANY): step data.
+    def addStep(self, index, step):
+        
+        if self._steps[index]:
+            e = "index %d is present." % index
+            #should find a way to get debug status.
+            #I know optparser is my friend :)
+#            if DEBUG: e += "\nData: %s" % self._steps[index]
+            raise YaliError, e
+
+        self._steps[index] = step
+
+        if not self._current:
+            self.setCurrent(index)
+
+    ##
+    # Set the current step. This can be overriden by the implementors.
+    # @param index(int): index of the step to be the current
+    def setCurrent(self, index):
+        self._current = index
+
+    ##
+    # Get current step data
+    def getCurrent(self):
+        return self._steps[self._current]
+
+    ##
+    # Get the current index number.
+    def getCurrentIndex(self):
+        return self._current
