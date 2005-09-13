6cdae1a9730cb8a2a7244370dfbe1352d26fd8c8
Baris Metin <bmetin@yelp.com>
2005-09-13 13:37:53 +0000

    - some code to test :).
    We don't have unittests now. One can argue if we need them... Anyway,
    I like unittests and considering to add. In PISI they helped a lot ;).

---
+++ yali/tests/print-all-partitions.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import sys
+sys.path.append(".")
+sys.path.append("..")
+
+import yali.storage
+
+def main():
+    devs = yali.storage.detect_all()
+
+    for dev in devs:
+        d = yali.storage.Device(dev)
+        d.set_partitions_from_disk()
+
+        print d.get_device(), ":", d.get_model()
+        for part in d.get_partitions().itervalues():
+            print "\t", part.get_minor(), part.get_type(), part.get_mb()
+
+if __name__ == "__main__":
+    main()
