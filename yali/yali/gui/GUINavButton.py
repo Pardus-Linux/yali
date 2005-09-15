399524bde03b3a1086a4a3b9714db86d535b6522
gurer <gurer@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2005-09-15 10:58:48 +0000

    navigasyon butonları transparent olsun

--- yali/yali/gui/GUINavButton.py
+++ yali/yali/gui/GUINavButton.py
@@ -27,12 +27,14 @@ class NavButton(QWidget):
         # don't use fixed sizes
         # find a way to paint button background.
         self.setFixedSize(64, 64)
-        self.setPaletteBackgroundPixmap(QPixmap())
+        #self.setPaletteBackgroundPixmap(QPixmap())
 
     def setIcon(self, icon_name):
         # FIXME: don't hard-code paths!!!
         icon_path = "pics/" + icon_name
         pix = QPixmap(icon_path)
+        self.bmap = pix.mask()
+        self.setMask(self.bmap)
         self.label.setPixmap(pix)
 
     def mouseReleaseEvent(self, e):
