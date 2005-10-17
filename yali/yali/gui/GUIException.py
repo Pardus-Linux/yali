463236c68b4baea215431059164d8b7d60e16adb
Baris Metin <bmetin@yelp.com>
2005-10-17 17:45:28 +0000

    forgotten file..

---
+++ yali/yali/gui/GUIException.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2005, TUBITAK/UEKAE
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+#
+# Please read the COPYING file.
+#
+
+from yali.exception import *
+
+class GUIError(YaliError):
+    pass
+
+class GUIException(YaliException):
+    pass
