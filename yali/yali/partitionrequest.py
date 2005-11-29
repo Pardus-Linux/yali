470fe06da7da492a5628d0ffbe52f2e1bbda9557
Baris Metin <bmetin@yelp.com>
2005-11-29 12:34:23 +0000

    fixy for #911

--- yali/yali/partitionrequest.py
+++ yali/yali/partitionrequest.py
@@ -50,7 +50,7 @@ class RequestList(list):
 
         # mount others
         for r in self.searchReqType(mountRequestType):
-            if r != rootreq:
+            if r.requestType() != rootreq:
                 r.applyRequest()
 
     ##
