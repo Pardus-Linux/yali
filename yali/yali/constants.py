4109b57eeb299796c25c8f6f1b91d24967539c99
Baris Metin <bmetin@yelp.com>
2005-12-15 16:36:53 +0000

    fix early comar bug. change version

--- yali/yali/constants.py
+++ yali/yali/constants.py
@@ -39,7 +39,7 @@ class Constants:
 
     def __init__(self):
 
-        self.__c.pardus_version = "Pardus 1.0 Beta"
+        self.__c.pardus_version = "Pardus 1.0 RC"
 
         self.__c.log_file = "/tmp/install.log"
 
