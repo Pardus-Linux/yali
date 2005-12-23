95a35f4bd44cc9385ecf608055aea58091d45381
A. Murat Eren <a.murat.eren@gmail.com>
2005-12-23 22:48:35 +0000

    object oriented version of bootloader.py (also change default timeout of grub to 10 secs)..

--- yali/yali/bootloader.py
+++ yali/yali/bootloader.py
@@ -21,15 +21,16 @@ _ = __trans.ugettext
 
 from yali.constants import consts
 
-grub_conf_tmp = """
+grub_conf_tmp = """\
 default 0
-timeout 5
+timeout 10
 splashimage = (%(grub_root)s)/boot/grub/splash.xpm.gz
 
 title=  %(pardus_version)s
 root (%(grub_root)s)
 kernel (%(grub_root)s)/boot/%(boot_kernel)s %(boot_parameters)s
 initrd=(%(grub_root)s)/boot/%(initrd)s 
+
 """
 
 win_part_tmp = """
@@ -56,111 +57,109 @@ root (%(grub_root)s)
 setup (%(grub_dev)s)
 """
 
-device_map = os.path.join(consts.target_dir, "boot/grub/device.map")
-grub_conf = os.path.join(consts.target_dir, "boot/grub/grub.conf")
+class BootLoader:
+    def __init__(self):
+        self.device_map = os.path.join('/', "boot/grub/device.map")
+        self.grub_conf = os.path.join('/', "boot/grub/grub.conf")
+        self.install_root = ''
+        self.install_dev = ''
 
-def _find_grub_dev(dev):
-    global device_map
+        self.win_dev = ''
+        self.win_root = ''
+        self.win_fs = ''
 
-    for l in open(device_map).readlines():
-        if l.find(dev) >= 0:
-            l = l.split()
-            d = l[0]
-            # remove paranthesis
-            return d[1:-1]
+    def _find_grub_dev(self, dev):
+        for l in open(self.device_map).readlines():
+            if l.find(dev) >= 0:
+                l = l.split()
+                d = l[0]
+                # remove paranthesis
+                return d[1:-1]
 
-
-def write_grub_conf(root, dev):
-    global device_map
-    global grub_conf
-
-    grub_dir = os.path.join(consts.target_dir, "boot/grub")
-    if not os.path.exists(grub_dir):
-        os.makedirs(grub_dir)
-
-    #write an empty grub.conf, for grub to create a device map.
-    open(grub_conf, "w").close()
-    # create device map
-    cmd = "/sbin/grub --batch --no-floppy --device-map=%s < %s" %(
-        device_map, grub_conf)
-    os.system(cmd)
-
-
-# TODO: support installing grub to diffrent devices's MBR.
-#    grub_dev = _find_grub_dev(dev)
-    minor = str(int(filter(lambda u: u.isdigit(), root)) -1)
-    grub_root = ",".join(["hd0", minor])
-
-
-    def find_boot_kernel():
-        d = os.path.join(consts.target_dir, "boot")
-        k = glob.glob(d + "/kernel-*")
-        return os.path.basename(k[0])
+    def write_grub_conf(self):
+        grub_dir = os.path.join('/', "boot/grub")
+        if not os.path.exists(grub_dir):
+            os.makedirs(grub_dir)
     
-    def find_initrd_name(bk):
-        ver = bk[len("kernel-"):]
-        return "initrd-%s" % ver
+        #write an empty grub.conf, for grub to create a device map.
+        open(self.grub_conf, "w").close()
+        # create device map
+        cmd = "/sbin/grub --batch --no-floppy --device-map=%s < %s" % \
+                                (self.device_map, self.grub_conf)
+        os.system(cmd)
 
-    def boot_parameters_from_cmdline(root):
-        s = ""
-        for i in  open("/proc/cmdline", "r").read().split():
-            if i.startswith("root="):
-                i = "root=/dev/%s" % root
-            elif i.startswith("cdroot") or i.startswith("init="):
-                continue
-
-            s = " ".join((s,i))
-
-        return s
-
-    boot_kernel = find_boot_kernel()
-    initrd_name = find_initrd_name(boot_kernel)
-    boot_parameters =  boot_parameters_from_cmdline(root)
-    s = grub_conf_tmp % {"root": root,
-                         "grub_root": grub_root,
-                         "pardus_version": consts.pardus_version,
-                         "boot_kernel": boot_kernel,
-                         "boot_parameters": boot_parameters,
-                         "initrd": initrd_name}
-    open(grub_conf, "w").write(s)
+        # TODO: support installing grub to diffrent devices's MBR.
+        #grub_dev = _find_grub_dev(self.install_dev)
 
 
-def grub_conf_append_win(root, win_dev, install_dev, fs):
-    global grub_conf
+        minor = str(int(filter(lambda u: u.isdigit(), self.install_root)) -1)
+        grub_root = ",".join(["hd0", minor])
 
-    grub_dev = _find_grub_dev(win_dev)
-    minor = str(int(filter(lambda u: u.isdigit(), root)) -1)
-    grub_root = ",".join([grub_dev, minor])
-
-    if win_dev == install_dev:
-        s = win_part_tmp % {"title": _("Windows"),
-                            "grub_root": grub_root,
-                            "root": root,
-                            "fs": fs}
-    else:
-        s = win_part_tmp_multiple_disks % {"title": _("Windows"),
-                                           "grub_root": grub_root,
-                                           "root": root,
-                                           "fs": fs}
-
-    open(grub_conf, "a").write(s)
+        def find_boot_kernel():
+            d = os.path.join('/', "boot")
+            k = glob.glob(d + "/kernel-*")
+            return os.path.basename(k[0])
+        
+        def find_initrd_name(bk):
+            ver = bk[len("kernel-"):]
+            return "initrd-%s" % ver
     
+        def boot_parameters_from_cmdline(root):
+            s = ""
+            for i in  open("/proc/cmdline", "r").read().split():
+                if i.startswith("root="):
+                    i = "root=/dev/%s" % root
+                elif i.startswith("cdroot") or i.startswith("init="):
+                    continue
+                s = " ".join((s,i))
+            return s
+    
+        boot_kernel = find_boot_kernel()
+        initrd_name = find_initrd_name(boot_kernel)
+        boot_parameters =  boot_parameters_from_cmdline(self.install_root)
+        s = grub_conf_tmp % {"root": self.install_root,
+                             "grub_root": grub_root,
+                             "pardus_version": consts.pardus_version,
+                             "boot_kernel": boot_kernel,
+                             "boot_parameters": boot_parameters,
+                             "initrd": initrd_name}
+        open(self.grub_conf, "w").write(s)
 
 
-def install_grub(root, dev):
 
-    grub_dev = _find_grub_dev(dev)
-    minor = str(int(filter(lambda u: u.isdigit(), root)) -1)
-    grub_root = ",".join([grub_dev, minor])
-
-    grub_shell = grub_shell_tmp % {"grubs_disk": '/dev/' + dev,
-                                   "grub_root": grub_root,
-                                   "grub_dev": grub_dev}
-
-    open("/tmp/grub-shell", "w").write(grub_shell)
-
-    # FIXME: check command...
-    cmd = "/sbin/grub --batch < /tmp/grub-shell"
-    print cmd
-    os.system(cmd)
+    def grub_conf_append_win(self):
+        grub_dev = self._find_grub_dev(self.win_dev)
+        minor = str(int(filter(lambda u: u.isdigit(), self.win_root)) -1)
+        grub_root = ",".join([grub_dev, minor])
+    
+        if self.win_dev == self.install_dev:
+            s = win_part_tmp % {"title": _("Windows"),
+                                "grub_root": grub_root,
+                                "root": self.win_root,
+                                "fs": self.win_fs}
+        else:
+            s = win_part_tmp_multiple_disks % {"title": _("Windows"),
+                                               "grub_root": grub_root,
+                                               "root": self.win_root,
+                                               "fs": self.win_fs}
+    
+        open(self.grub_conf, "a").write(s)
+        
+    
+    
+    def install_grub(self):
+        grub_dev = self._find_grub_dev(self.install_dev)
+        minor = str(int(filter(lambda u: u.isdigit(), self.install_root)) -1)
+        grub_root = ",".join([grub_dev, minor])
+    
+        grub_shell = grub_shell_tmp % {"grubs_disk": '/dev/' + self.install_dev,
+                                       "grub_root": grub_root,
+                                       "grub_dev": grub_dev}
+    
+        open("/tmp/grub-shell", "w").write(grub_shell)
+    
+        # FIXME: check command...
+        cmd = "/sbin/grub --batch < /tmp/grub-shell"
+        print cmd
+        os.system(cmd)
 
