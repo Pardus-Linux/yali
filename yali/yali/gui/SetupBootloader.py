dffabf265a9a962a0be8842e5ea8e6592998a536
Baris Metin <bmetin@yelp.com>
2005-12-24 07:53:02 +0000

    fixy

--- yali/yali/gui/SetupBootloader.py
+++ yali/yali/gui/SetupBootloader.py
@@ -103,7 +103,7 @@ loader.
         rootreq = ctx.partrequests.searchPartTypeAndReqType(parttype.root,
                                                             request.mountRequestType).next()
 
-        loader.install_dev = self.device.getDevicePath()
+        loader.install_dev = self.device.getPath()
         loader.install_root = basename(rootreq.partition().getPath())
         
         # TODO: use logging!
