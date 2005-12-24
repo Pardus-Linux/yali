c647d93a4ead724185e848ec48ba4d591ef68760
Baris Metin <bmetin@yelp.com>
2005-12-24 08:38:40 +0000

    hola

--- yali/yali/constants.py
+++ yali/yali/constants.py
@@ -39,7 +39,7 @@ class Constants:
 
     def __init__(self):
 
-        self.__c.pardus_version = "Pardus 1.0 RC"
+        self.__c.pardus_version = "Pardus 1.0"
 
         self.__c.log_file = "/tmp/install.log"
 
