ced2a0ca218de06197bb43ab11627a2e2c69f58d
Baris Metin <bmetin@yelp.com>
2006-05-08 16:20:50 +0000

    fixy firefox ;)

--- yali/yali/postinstall.py
+++ yali/yali/postinstall.py
@@ -59,4 +59,5 @@ def initbaselayout():
     src = "/etc/X11/xorg.conf"
     dst = os.path.join(consts.target_dir, "etc/X11/xorg.conf")
     if os.path.exists(src):
+        os.makedirs(os.path.dirname(dst))
         shutil.copyfile(src, dst)
