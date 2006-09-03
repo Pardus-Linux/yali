308790eed3264672b3574abec4d088203e4fdc9b
S.Çağlar Onur <caglar10ur@users.noreply.github.com>
2006-09-03 20:47:46 +0000

    yeni gruplara alış ey yalı :P

--- yali/yali/gui/SetupUsers.py
+++ yali/yali/gui/SetupUsers.py
@@ -110,7 +110,7 @@ Click Next button to proceed.
         # ignore last character. see bug #887
         u.realname = unicode(self.realname.text().utf8().data())[:-1]
         u.passwd = self.pass1.text().ascii()
-        u.groups = ["users", "cdrom", "audio", "lp", "video", "haldaemon", "plugdev", "wheel", "dialout", "uucp", "scanner"]
+        u.groups = ["users", "pnp", "audio", "video", "wheel", "dialout", "uucp"]
 
 
         existsInList = [i for i in range(self.userList.count())
