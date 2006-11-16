2ec0639834b6c81d21d46e38e302fe16e62d2f21
S.Çağlar Onur <caglar10ur@users.noreply.github.com>
2006-11-16 17:33:12 +0000

    new group for cups

--- yali/yali/gui/ScrUsers.py
+++ yali/yali/gui/ScrUsers.py
@@ -125,7 +125,7 @@ Click Next button to proceed.
         # ignore last character. see bug #887
         u.realname = unicode(self.realname.text().utf8().data())[:-1]
         u.passwd = self.pass1.text().ascii()
-        u.groups = ["users", "pnp", "removable", "disk", "audio", "video", "power", "wheel", "dialout"]
+        u.groups = ["users", "pnp", "pnpadmin", "removable", "disk", "audio", "video", "power", "wheel", "dialout"]
 
 
         existsInList = [i for i in range(self.userList.count())
