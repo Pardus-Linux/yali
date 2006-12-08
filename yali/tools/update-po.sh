76489478202762f78dcffd6bdcba3527c6eb5c1c
Baris Metin <bmetin@yelp.com>
2006-12-08 21:24:45 +0000

    update po/pot

--- yali/tools/update-po.sh
+++ yali/tools/update-po.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-LANGUAGES="tr nl fr de es"
+LANGUAGES=`ls po/*.po`
 
 set -x
 
 xgettext -L "python" -k__tr -k_ yali/gui/Ui/*.py yali/gui/*.py yali/*.py -o po/yali.pot
 for lang in $LANGUAGES
 do
-    msgmerge -U po/$lang.po po/yali.pot
+    msgmerge -U $lang po/yali.pot
 done
 
