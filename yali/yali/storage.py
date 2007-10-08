b5d8143ef068a77d8cd0f842ddf4afd08b1e6b0b
Gökmen Göksel <gokmen@goksel.me>
2007-10-08 10:38:37 +0000

    Edd module for finding the bootable disk, from Onur ;)

--- yali/yali/storage.py
+++ yali/yali/storage.py
@@ -444,6 +444,76 @@ class Device:
         # pyparted will do it for us.
         del self._disk
 
+# Edd Module Class, for getting BIOS Boot order for disks..
+class EDD:
+    def __init__(self):
+        self.edd_dir = "/sys/firmware/edd"
+        self.edd_offset = 440
+        self.edd_len = 4
+
+    def blockDevices(self):
+        devices = []
+        for dev_type in ["hd*", "sd*"]:
+            sysfs_devs = glob.glob("/sys/block/" + dev_type)
+            for sysfs_dev in sysfs_devs:
+                if not int(open(sysfs_dev + "/removable").read().strip()):
+                    devlink = os.readlink(sysfs_dev + "/device")
+                    if not "/usb" in devlink:
+                        devices.append("/dev/" + os.path.basename(sysfs_dev))
+        devices.sort()
+        return devices
+
+    def match_sys(self, _a):
+        b = struct.unpack("2s2s2s2s", _a)
+        return "0x"+b[3]+b[2]+b[1]+b[0]
+
+    def get_edd_sig(self, _n):
+        sigfile = "%s/int13_dev%s/mbr_signature" % (self.edd_dir, _n)
+        if os.path.exists(sigfile):
+            sig = file(sigfile).read().strip("\n")
+        else:
+            sig = "NOT FOUND"
+
+        return sig
+
+    def get_mbr_sig(self, _f):
+        f = file(_f)
+        f.seek(self.edd_offset)
+        a = f.read(self.edd_len)
+        f.close()
+
+        sig = self.match_sys(binascii.b2a_hex(a))
+        return sig
+
+    def list_edd_signatures(self):
+        sigs = {}
+        if os.path.exists(self.edd_dir):
+            for d in os.listdir(self.edd_dir):
+                bios_num = d[9:]
+                sigs[bios_num] = self.get_edd_sig(bios_num)
+        else:
+            raise DeviceError, "Edd module not installed !"
+        return sigs
+
+    def list_mbr_signatures(self):
+        sigs = {}
+        for d in self.blockDevices():
+            sigs[self.get_mbr_sig(d)] = d
+        return sigs
+
+def getOrderedDiskList():
+    edd = EDD()
+    sortedList = []
+    edd_list = edd.list_edd_signatures()
+    mbr_list = edd.list_mbr_signatures()
+    edd_keys = edd_list.keys()
+    edd_keys.sort()
+    for bios_num in edd_keys:
+        edd_sig = edd_list[bios_num]
+        sortedList.append(mbr_list[edd_sig])
+    return sortedList
+
+
 ##
 # Return a list of block devices in system
 def detect_all():
