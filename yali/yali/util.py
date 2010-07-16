016c13762d7213c88f3176c62e15367ce266bdf3
Mete Alpaslan Katırcıoğlu <mete@katircioglu.net>
2010-07-16 12:50:12 +0000

    Somae function added in util module
    To determine swap size for using swap size suggestion and getting architecture
    information to detect parted label type are added.

--- yali/yali/util.py
+++ yali/yali/util.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 import subprocess
 import yali.context as ctx
+from pardus.diskutils import EDD
 
 class Singleton(type):
     def __init__(cls, name, bases, dict):
@@ -13,6 +14,25 @@ class Singleton(type):
             cls.instance = super(Singleton, cls).__call__(*args, **kw)
 
         return cls.instance
+
+def get_edd_dict(devices):
+    eddDevices = {}
+
+    if not os.path.exists("/sys/firmware/edd"):
+        rc = run_batch("modprobe edd")[0]
+        if rc > 0:
+            ctx.debugger.error("Inserting EDD Module failed !")
+            return eddDevices
+        else:
+            edd = EDD()
+            edds = edd.list_edd_signatures()
+            mbrs = edd.list_mbr_signatures()
+
+            for number, signature in edds.items():
+                if mbrs.has_key(signature):
+                    if mbrs[signature] in devices:
+                        eddDevices[mbrs[signature]] = number
+            return eddDevices
 
 def run_batch(cmd):
     """Run command and report return value and output."""
@@ -49,3 +69,68 @@ def run_logged(cmd):
     ctx.ui.debug(_('return value for "%s" is %s') % (cmd, p.returncode))
 
     return p.returncode
+
+efi = None
+def isEfi():
+    global efi
+    if efi is not None:
+        return efi
+
+    efi = False
+    if os.path.exists("/sys/firmware/efi"):
+        efi = True
+
+    return efi
+
+def isX86(bits=None):
+    arch = os.uname()[4]
+
+    # x86 platforms include:
+    #     i*86
+    #     x86_64
+    if bits is None:
+        if (arch.startswith('i') and arch.endswith('86')) or or arch == 'x86_64':
+            return True
+    elif bits == 32:
+        if arch.startswith('i') and arch.endswith('86'):
+            return True
+    elif bits == 64:
+        if arch == 'x86_64':
+            return True
+
+    return False
+
+def memInstalled():
+    f = open("/proc/meminfo", "r")
+    lines = f.readlines()
+    f.close()
+
+    for l in lines:
+        if l.startswith("MemTotal:"):
+            fields = string.split(l)
+            mem = fields[1]
+            break
+
+    return int(mem)
+
+def swapSuggestion(quiet=0):
+    mem = memInstalled()/1024
+    mem = ((mem/16)+1)*16
+    if not quiet:
+        ctx.logger.info("Detected %sM of memory", mem)
+
+    if mem <= 256:
+        minswap = 256
+        maxswap = 512
+    else:
+        if mem > 2048:
+            minswap = 1024
+            maxswap = 2048 + mem
+        else:
+            minswap = mem
+            maxswap = 2*mem
+
+    if not quiet:
+        ctx.logger.info("Swap attempt of %sM to %sM", minswap, maxswap)
+
+    return (minswap, maxswap)
