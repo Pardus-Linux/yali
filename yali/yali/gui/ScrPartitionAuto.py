8bbca5fe3732bad53837648e2c4680509a798aef
Mete Alpaslan Katırcıoğlu <mete@katircioglu.net>
2010-08-19 19:18:09 +0000

    Enable clearPartDisks widget
    Activate selecting disk for storage operations.

--- yali/yali/gui/ScrPartitionAuto.py
+++ yali/yali/gui/ScrPartitionAuto.py
@@ -20,12 +20,12 @@ from PyQt4 import QtGui
 from PyQt4.QtCore import *
 
 import yali.context as ctx
-from yali.gui.ScreenWidget import ScreenWidget
-from yali.gui.GUIException import GUIException
+from yali.gui.ScreenWidget import ScreenWidget, GUIError
 from yali.gui.Ui.autopartwidget import Ui_AutoPartWidget
 from yali.gui.Ui.partitionshrinkwidget import Ui_PartShrinkWidget
 from yali.storage.partitioning import CLEARPART_TYPE_ALL, CLEARPART_TYPE_LINUX, CLEARPART_TYPE_NONE, doAutoPartition, defaultPartitioning
 from yali.storage.operations import OperationResizeDevice, OperationResizeFormat
+
 
 class ShrinkWidget(QtGui.QWidget):
     def __init__(self, parent):
@@ -34,14 +34,30 @@ class ShrinkWidget(QtGui.QWidget):
         self.ui = Ui_PartShrinkWidget()
         self.ui.setupUi(self)
         self.setStyleSheet("""
-                QFrame#mainFrame {
-                    background-image: url(:/gui/pics/transBlack.png);
-                    border: 1px solid #BBB;
-                    border-radius:8px;
-                }
-                QWidget#Ui_PartShrinkWidget {
-                    background-image: url(:/gui/pics/trans.png);
-                }
+                     QSlider::groove:horizontal {
+                         border: 1px solid #999999;
+                         height: 12px;
+                         background: qlineargradient(x1:0, y1:0, x2:0, y2:1, stop:0 #B1B1B1, stop:1 #c4c4c4);
+                         margin: 2px 0;
+                     }
+
+                     QSlider::handle:horizontal {
+                         background: qlineargradient(x1:0, y1:0, x2:1, y2:1, stop:0 #b4b4b4, stop:1 #8f8f8f);
+                         border: 1px solid #5c5c5c;
+                         width: 18px;
+                         margin: 0 0;
+                         border-radius: 2px;
+                     }
+
+                    QFrame#mainFrame {
+                        background-image: url(:/gui/pics/transBlack.png);
+                        border: 1px solid #BBB;
+                        border-radius:8px;
+                    }
+
+                    QWidget#Ui_PartShrinkWidget {
+                        background-image: url(:/gui/pics/trans.png);
+                    }
         """)
         self.operations = []
         QObject.connect(self.ui.partitions, SIGNAL("currentRowChanged(int)"), self.updateSpin)
@@ -75,8 +91,13 @@ class ShrinkWidget(QtGui.QWidget):
 
     def updateSpin(self, index):
         request = self.ui.partitions.item(index).partition
-        reqlower = long(math.ceil(request.format.minSize))
-        requpper = long(math.floor(request.format.currentSize))
+        try:
+            reqlower = long(math.ceil(request.format.minSize))
+        except FilesystemError, msg:
+            raise GUIError, msg
+        else:
+            requpper = long(math.floor(request.format.currentSize))
+
         self.ui.shrinkMB.setMinimum(max(1, reqlower))
         self.ui.shrinkMB.setMaximum(requpper)
         self.ui.shrinkMB.setValue(reqlower)
@@ -113,12 +134,51 @@ class ShrinkWidget(QtGui.QWidget):
 
         self.hide()
 
-class DriveItem(QtGui.QListWidgetItem):
+class DrivesListItem(QtGui.QListWidgetItem):
+    def __init__(self, parent, widget):
+        QtGui.QListWidgetItem.__init__(self, parent)
+        self.widget = widget
+        self.setSizeHint(QSize(300, 64))
+
+class DriveItem(QtGui.QWidget):
     def __init__(self, parent, drive):
-        text = u"%s on %s (%s) MB" % (drive.model, drive.name, str(int(drive.size)))
-        QtGui.QListWidgetItem.__init__(self, text, parent)
+        QtGui.QWidget.__init__(self, parent)
+        #self.gridLayout = QtGui.QGridLayout(self)
+        self.layout = QtGui.QHBoxLayout(self)
+        self.checkBox = QtGui.QCheckBox(self)
+        #self.gridLayout.addWidget(self.checkBox, 0, 0, 1, 1)
+        #self.layout.addWidget(self.checkBox, 0, 0, 1, 1)
+        self.layout.addWidget(self.checkBox)
+        #self.verticalLayout = QtGui.QVBoxLayout()
+        self.labelDrive = QtGui.QLabel(self)
+        self.labelDrive.setText("%s on %s - (%s) MB" % (drive.model, drive.name, str(int(drive.size))))
+        self.layout.addWidget(self.labelDrive)
+        spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        self.layout.addItem(spacerItem)
+        #self.labelDescription = QtGui.QLabel(self)
+        #self.labelDescription.setText("%s" % drive.model)
+        #self.verticalLayout.addWidget(self.labelDrive)
+        #self.verticalLayout.addWidget(self.labelDescription)
+        #self.gridLayout.addLayout(self.verticalLayout, 0, 1, 1, 1)
+        self.connect(self.checkBox, SIGNAL("stateChanged(int)"), self.stateChanged)
         self.drive = drive
-        self.setCheckState(Qt.Unchecked)
+        self.parent = parent
+
+    def stateChanged(self, state):
+        if state == Qt.Checked:
+            ctx.mainScreen.enableNext()
+        else:
+            selectedDisks = []
+            for index in range(self.parent.count()):
+                if self.checkBox.checkState() == Qt.Checked:
+                    selectedDisks.append(self.ui.drives.item(index).drive)
+
+            if len(selectedDisks):
+                ctx.mainScreen.enableNext()
+            else:
+                ctx.mainScreen.disableNext()
+
+
 
 class PartitionItem(QtGui.QListWidgetItem):
 
@@ -152,16 +212,16 @@ Pardus create a new partition for installation.</p>
         self.storage = ctx.storage
         self.intf = ctx.yali
         self.shrinkOperations = None
+        self.clearPartDisks = None
 
         self.connect(self.ui.useAllSpace, SIGNAL("clicked()"), self.typeChanged)
         self.connect(self.ui.replaceExistingLinux, SIGNAL("clicked()"), self.typeChanged)
         self.connect(self.ui.shrinkCurrent, SIGNAL("clicked()"), self.typeChanged)
         self.connect(self.ui.useFreeSpace, SIGNAL("clicked()"), self.typeChanged)
         self.connect(self.ui.createCustom, SIGNAL("clicked()"), self.typeChanged)
-        #self.connect(self.ui.review, SIGNAL("stateChanged(int) "), self.typeChanged)
         #self.connect(self.ui.drives,   SIGNAL("currentItemChanged(QListWidgetItem *, QListWidgetItem * )"),self.slotDeviceChanged)
-        self.ui.drives.hide()
-        self.ui.drivesLabel.hide()
+        #self.ui.drives.hide()
+        #self.ui.drivesLabel.hide()
 
     def typeChanged(self):
         if self.sender() != self.ui.createCustom:
@@ -198,7 +258,9 @@ Pardus create a new partition for installation.</p>
 
         for disk in disks:
             if disk.size >= ctx.consts.min_root_size:
-                DriveItem(self.ui.drives, disk)
+                drive = DriveItem(self.ui.drives, disk)
+                listItem = DrivesListItem(self.ui.drives, drive)
+                self.ui.drives.setItemWidget(listItem, drive)
 
         # select the first disk by default
         self.ui.drives.setCurrentRow(0)
@@ -206,28 +268,27 @@ Pardus create a new partition for installation.</p>
     def shown(self):
         self.storage.reset()
         if self.storage.checkNoDisks(self.intf):
-            raise GUIException, _("No storage device found.")
+            raise GUIError, _("No storage device found.")
         else:
             self.fillDrives()
             self.setPartitioningType()
 
-    def checkSelectedDisk(self):
-        clearDisks = []
+    def checkClearPartDisks(self):
+        selectedDisks = []
         for index in range(self.ui.drives.count()):
-            if self.ui.drives.item(index).checkState() == Qt.Checked:
-                clearDisks.append(self.ui.drives.item(index).drive)
+            if self.ui.drives.item(index).widget.checkBox.checkState() == Qt.Checked:
+                selectedDisks.append(self.ui.drives.item(index).widget.drive)
 
-        if len(clearDisks) == 0:
+        if len(selectedDisks) == 0:
             self.intf.messageWindow(_("Error"),
                                     _("You must select at least one "
                                       "drive to be used for installation."), customIcon="error")
-            ctx.mainScreen.disableNext()
+            return False
 
-        clearDisks.sort(self.storage.compareDisks)
-        self.storage.clearPartDisks = clearDisks
+        self.clearPartDisks = selectedDisks.sort(self.storage.compareDisks)
+        return True
 
     def execute(self):
-        #self.checkSelectedDisk()
         rc = self.nextCheck()
         if rc is None:
             #FIXME:Unknown bug
@@ -237,30 +298,34 @@ Pardus create a new partition for installation.</p>
             return rc
 
     def nextCheck(self):
-        increment = 0
-        if self.ui.createCustom.isChecked():
-            ctx.mainScreen.stepIncrement = 1
-            self.storage.clearPartType = CLEARPART_TYPE_NONE
-        else:
-            if self.ui.shrinkCurrent.isChecked():
-                if self.shrinkOperations:
-                    for operation in self.shrinkOperations:
-                        self.storage.addOperation(operation)
-                    self.storage.clearPartType = CLEARPART_TYPE_NONE
-            elif self.ui.useAllSpace.isChecked():
-                self.storage.clearPartType = CLEARPART_TYPE_ALL
-            elif self.ui.replaceExistingLinux.isChecked():
-                self.storage.clearPartType = CLEARPART_TYPE_LINUX
-            elif self.ui.useFreeSpace.isChecked():
+        if self.checkClearPartDisks():
+            increment = 0
+            if self.ui.createCustom.isChecked():
+                ctx.mainScreen.stepIncrement = 1
                 self.storage.clearPartType = CLEARPART_TYPE_NONE
-
-            self.storage.doAutoPart = True
-            self.storage.autoPartitionRequests = defaultPartitioning(self.storage, quiet=0)
-
-            if self.ui.review.isChecked():
-                increment = 1
             else:
-                increment = 2
+                if self.ui.shrinkCurrent.isChecked():
+                    if self.shrinkOperations:
+                        for operation in self.shrinkOperations:
+                            self.storage.addOperation(operation)
+                        self.storage.clearPartType = CLEARPART_TYPE_NONE
+                elif self.ui.useAllSpace.isChecked():
+                    self.storage.clearPartType = CLEARPART_TYPE_ALL
+                elif self.ui.replaceExistingLinux.isChecked():
+                    self.storage.clearPartType = CLEARPART_TYPE_LINUX
+                elif self.ui.useFreeSpace.isChecked():
+                    self.storage.clearPartType = CLEARPART_TYPE_NONE
 
-        ctx.mainScreen.stepIncrement = increment
-        return doAutoPartition(self.storage)
+                self.storage.doAutoPart = True
+                self.storage.autoPartitionRequests = defaultPartitioning(self.storage, quiet=0)
+                self.storage.clearPartDisks = self.clearPartDisks
+
+                if self.ui.review.isChecked():
+                    increment = 1
+                else:
+                    increment = 2
+
+            ctx.mainScreen.stepIncrement = increment
+            return doAutoPartition(self.storage)
+
+        return False
