0fbcba3ff6e49e95a54bed2cc746e25fb6e8c0e6
Mete Alpaslan Katırcıoğlu <mete@katircioglu.net>
2010-09-13 22:22:56 +0000

    storage.library.swap:Move swap format relational methods
    Move swap relational methods from util to storage.library.swap to
    collect in storage library.

--- yali/yali/util.py
+++ yali/yali/util.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import os
-import resource
 import subprocess
 import gettext
 
@@ -14,15 +13,6 @@ import yali.context as ctx
 from pardus.diskutils import EDD
 
 EARLY_SWAP_RAM = 512 * 1024 # 512 MB
-
-class SwapError(yali.Error):
-    pass
-
-class OldSwapError(SwapError):
-    pass
-
-class UnknownSwapError(SwapError):
-    pass
 
 def product_name():
     if os.path.exists("/etc/pardus-release"):
@@ -204,88 +194,6 @@ def mkdirChain(dir):
             pass
 
         ctx.logger.error("could not create directory %s: %s" % (dir, e.strerror))
-
-def mkswap(device, label=''):
-    # We use -f to force since mkswap tends to refuse creation on lvs with
-    # a message about erasing bootbits sectors on whole disks. Bah.
-    argv = ["-f"]
-    if label:
-        argv.extend(["-L", label])
-    argv.append(device)
-
-    rc = run_batch("mkswap", argv)[0]
-
-    if rc:
-        raise SwapError("mkswap failed for '%s'" % device)
-
-def swapon(device, priority=None):
-    pagesize = resource.getpagesize()
-    buf = None
-    sig = None
-
-    if pagesize > 2048:
-        num = pagesize
-    else:
-        num = 2048
-
-    try:
-        fd = os.open(device, os.O_RDONLY)
-        buf = os.read(fd, num)
-    except OSError:
-        pass
-    finally:
-        try:
-            os.close(fd)
-        except (OSError, UnboundLocalError):
-            pass
-
-    if buf is not None and len(buf) == pagesize:
-        sig = buf[pagesize - 10:]
-        if sig == 'SWAP-SPACE':
-            raise OldSwapError
-        if sig == 'S1SUSPEND\x00' or sig == 'S2SUSPEND\x00':
-            raise SuspendError
-
-    if sig != 'SWAPSPACE2':
-        raise UnknownSwapError
-
-    argv = []
-    if isinstance(priority, int) and 0 <= priority <= 32767:
-        argv.extend(["-p", "%d" % priority])
-    argv.append(device)
-    rc = run_batch("swapon",argv)[0]
-
-    if rc:
-        raise SwapError("swapon failed for '%s'" % device)
-
-def swap_off(device):
-    rc = run_batch("swapoff", [device])[0]
-
-    if rc:
-        raise SwapError("swapoff failed for '%s'" % device)
-
-def swap_status(device):
-    alt_dev = None
-    if device.startswith("/dev/mapper/"):
-        # get the real device node for device-mapper devices since the ones
-        # with meaningful names are just symlinks
-        try:
-            alt_dev = "/dev/%s" % dm_node_from_name(device.split("/")[-1])
-        except DMError:
-            alt_dev = None
-
-    lines = open("/proc/swaps").readlines()
-    status = False
-    for line in lines:
-        if not line.strip():
-            continue
-
-        swap_dev = line.split()[0]
-        if swap_dev in [device, alt_dev]:
-            status = True
-            break
-
-    return status
 
 def swap_amount():
     f = open("/proc/meminfo", "r")
