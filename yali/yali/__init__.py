21c2523f449108aa9f2001e267d7b5c520126b37
Mete Alpaslan Katırcıoğlu <mete@katircioglu.net>
2010-10-31 11:21:57 +0000

    yali.__init__: Importing yali only initialize logging
    Yali initialize only logging system if it is purely used as a python
    package.

--- yali/yali/__init__.py
+++ yali/yali/__init__.py
@@ -9,38 +9,21 @@
 #
 # Please read the COPYING file.
 #
+import os
+import logging
+import logging.handlers
+import gettext
+_ = gettext.translation('yali', fallback=True).ugettext
+
+import yali.context as ctx
 
 class Error(Exception):
     pass
 
-import os
-import sys
-import traceback
-import cStringIO
-import logging
-import gettext
-import yali.context as ctx
-
-__trans = gettext.translation('yali', fallback=True)
-_ = __trans.ugettext
-
-import pisi
-
-class Singleton(type):
-    def __init__(cls, name, bases, dict):
-        super(Singleton, cls).__init__(name, bases, dict)
-        cls.instance = None
-
-    def __call__(cls, *args, **kw):
-        if cls.instance is None:
-            cls.instance = super(Singleton, cls).__call__(*args, **kw)
-
-        return cls.instance
-
-def init_logging(log_dir):
-    import yali.context as ctx
+def init_logging():
+    log_dir = os.path.join(ctx.consts.log_dir, ctx.consts.log_file)
     if os.access(log_dir, os.W_OK):
-        handler = logging.handlers.RotatingFileHandler('%s/yali.log' % log_dir)
+        handler = logging.handlers.RotatingFileHandler(log_dir)
         formatter = logging.Formatter('%(asctime)-12s: %(levelname)-8s %(message)s')
         handler.setFormatter(formatter)
         ctx.logger = logging.getLogger('yali')
@@ -48,40 +31,5 @@ def init_logging(log_dir):
         ctx.loghandler = handler
         ctx.logger.setLevel(logging.DEBUG)
 
-def default_runner():
-    """ Main runner of YALI """
-    import yali.gui.runner
-    sys.excepthook = exception_handler
-    return yali.gui.runner.Runner()
 
-exception_yali, exception_pisi, exception_unknown = range(3)
-
-def exception_handler(exception, value, tb):
-    """ YALI exception handler for showing exceptions in GUI """
-
-    knownExceptions = {Error:           exception_yali,
-                       pisi.Error:      exception_pisi}
-
-    try:
-        exception_type = knownExceptions[filter(lambda x: isinstance(value, x), knownExceptions.keys())[0]]
-    except IndexError:
-        exception_type = exception_unknown
-
-    sio = cStringIO.StringIO()
-
-    v = ''
-    for e in value.args:
-        v += str(e) + '\n'
-    sio.write(v)
-
-    sio.write(str(exception))
-    sio.write('\n\n')
-    sio.write(_("Backtrace:"))
-    sio.write('\n')
-    traceback.print_tb(tb, None, sio)
-
-    sio.seek(0)
-
-    import yali.gui.runner
-    yali.gui.runner.showException(exception_type, unicode(sio.read()))
-
+init_logging()
