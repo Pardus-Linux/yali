250d775f21b5ee4b7e08d4e86229fb2a01f73fa0
Mete Alpaslan Katırcıoğlu <mete@katircioglu.net>
2010-10-31 11:21:58 +0000

    yali.constants:Constans should be singleton
    Constants class is changed as a Singleton with metaclass usage.

--- yali/yali/constants.py
+++ yali/yali/constants.py
@@ -13,10 +13,20 @@
 # YALI constants module defines a class with constant members. An
 # object from this class can only bind values one to it's members.
 
+import os
 import locale
-from os.path import join
-from os.path import exists
-from yali.options import options
+
+class Singleton(type):
+    def __init__(cls, name, bases, dict):
+        super(Singleton, cls).__init__(name, bases, dict)
+        cls.instance = None
+
+    def __call__(cls, *args, **kw):
+        if cls.instance is None:
+            cls.instance = super(Singleton, cls).__call__(*args, **kw)
+
+        return cls.instance
+
 
 class _constant:
     """ Constant members implementation """
@@ -37,7 +47,32 @@ class _constant:
 
 class Constants:
     """ YALI's Constants """
+    __metaclass__ = Singleton
+
     __c = _constant()
+
+    def __init__(self):
+        self.__c.min_root_size = 3500
+        self.__c.data_dir = "/usr/share/yali"
+        self.__c.log_dir = "/var/log"
+        self.__c.log_file = "yali.log"
+        self.__c.target_dir = "/mnt/target"
+        self.__c.source_dir = "/mnt/cdrom"
+        self.__c.tmp_mnt_dir = "/tmp/check"
+        self.__c.dbus_socket = os.path.join(self.__c.target_dir,"var/run/dbus/system_bus_socket")
+        self.__c.slidepics_dir = os.path.join(self.__c.data_dir, "slideshow")
+        self.__c.repo_uri = os.path.join(self.__c.source_dir, "repo/pisi-index.xml.bz2")
+        self.__c.pisi_collection_file = os.path.join(self.__c.data_dir, "data/index/collection.xml")
+        self.__c.pisi_collection_dir = os.path.join(self.__c.data_dir, "data/index")
+        self.__c.cd_repo_name = os.path.join(self.__c.source_dir, "repo/pisi-index.xml.bz2")
+        self.__c.cd_repo_uri = os.path.join(self.__c.source_dir, "repo/pisi-index.xml.bz2")
+        self.__c.pardus_repo_uri = "http://packages.pardus.org.tr"
+        self.__c.pisi_index_file = os.path.join(self.__c.data_dir,"data/pisi-index.xml.bz2")
+        self.__c.session_file = os.path.join(self.__c.target_dir, "root/kahyaSession.xml")
+        self.__c.default_kahya_file = os.path.join(self.__c.data_dir,"data/default.xml")
+        self.__c.oem_install_file = os.path.join(self.__c.data_dir,"data/firstBoot.xml")
+        self.__c.alternatestylesheet = os.path.join(self.__c.data_dir, "data/oxygen.qss")
+        self.__c.lang = locale.getdefaultlocale()[0][:2]
 
     def __getattr__(self, attr):
         return getattr(self.__c, attr)
@@ -48,85 +83,5 @@ class Constants:
     def __delattr__(self, attr):
         delattr(self.__c, attr)
 
+
 consts = Constants()
-
-consts.pardus_version = file("/etc/pardus-release").readlines()[0].strip()
-
-consts.data_dir = "/usr/share/yali"
-
-consts.mnt_dir = "/mnt"
-
-consts.tmp_mnt_dir = "/tmp/check"
-
-if options.firstBoot == True or exists("/etc/yali-is-firstboot"):
-    consts.target_dir = "/"
-else:
-    # new system will be installed directly into this target directory
-    consts.target_dir = join(consts.mnt_dir, "target")
-
-# log file for storing after installation
-consts.log_file = join(consts.target_dir,"var/log/yaliInstall.log")
-
-# session file for storing after installation
-consts.session_file = join(consts.target_dir,"root/kahyaSession.xml")
-
-# packages (and maybe others) will be in this source (cdrom) directory
-consts.source_dir = join(consts.mnt_dir, "cdrom")
-
-# dbus socket path
-consts.dbus_socket_file = join(consts.target_dir, "var/run/dbus/system_bus_socket")
-
-# swap file path
-consts.swap_file_name = ".swap"
-consts.swap_file_path = join(consts.target_dir, 
-                             consts.swap_file_name)
-
-# user faces (for KDM)
-consts.user_faces_dir = join(consts.data_dir, "user_faces")
-
-# pisi repository
-consts.cd_repo_name = "pardus-cd"
-consts.cd_repo_uri = join(consts.source_dir, "repo/pisi-index.xml.bz2")
-
-# pardus repository
-consts.pardus_repo_name = "pardus-2009"
-consts.pardus_repo_uri = "http://packages.pardus.org.tr/pardus-2009/pisi-index.xml.bz2"
-consts.pardus_release_path = "etc/pardus-release"
-
-# min root partition size
-consts.min_root_size = 3500
-
-# kahya options
-consts.kahya_param = "kahya"
-consts.default_kahya_file = join(consts.data_dir,"data/default.xml")
-
-# oem install options
-consts.oem_install_param = "oeminstall"
-consts.oem_install_file = join(consts.data_dir,"data/firstBoot.xml")
-
-# rescue mode parameter for cmdline
-consts.rescue_mode_param = "rescue"
-
-# system.base packages only parameter for cmdline
-consts.base_only_param = "baseonly"
-
-# pisi index files
-consts.dvd_repo_name = "pardus-dvd"
-consts.dvd_install_param = "dvdinstall"
-consts.pisi_index_file = join(consts.data_dir,"data/pisi-index.xml.bz2")
-consts.pisi_index_file_sum = join(consts.data_dir,"data/pisi-index.xml.bz2.sha1sum")
-
-# pisi collection index file
-consts.pisi_collection_file = join(consts.data_dir, "data/index/collection.xml")
-consts.pisi_collection_dir = join(consts.data_dir, "data/index")
-
-# slideshows
-consts.slidepics_dir = join(consts.data_dir, "slideshow")
-
-try:
-    consts.lang = locale.getdefaultlocale()[0][:2]
-except:
-    # default lang to en_US
-    consts.lang = "en"
-
-consts.MAX_PART_SIZE = 1024*1024*1024
