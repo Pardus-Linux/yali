718274c3bff65243486eb9c1eba9fa8fe2ce5d28
Mete Alpaslan Katırcıoğlu <mete@katircioglu.net>
2010-10-31 11:58:16 +0000

    gui.ScrCheckCD:Move installer.checkCD method into this module
    Define required methods in area of usage if it is not neeed to access in context

--- yali/yali/gui/ScrCheckCD.py
+++ yali/yali/gui/ScrCheckCD.py
@@ -11,53 +11,119 @@
 #
 
 import gettext
-__trans = gettext.translation('yali', fallback=True)
-_ = __trans.ugettext
+_ = gettext.translation('yali', fallback=True).ugettext
 
-from PyQt4 import QtGui
-from PyQt4.QtCore import *
+from PyQt4.Qt import QWidget, SIGNAL, QIcon, QPixmap
 
 import pisi.ui
-import yali.pisiiface
-from yali.gui.ScreenWidget import ScreenWidget
-from yali.gui.Ui.checkcdwidget import Ui_CheckCDWidget
 import yali.context as ctx
+import yali.pisiiface
+from yali.gui import ScreenWidget, register_gui_screen
+from yali.gui.Ui.checkcdwidget import Ui_CheckCDWidget
 
 from yali.gui.YaliDialog import Dialog
 
-class Widget(QtGui.QWidget, ScreenWidget):
+class Widget(QWidget, ScreenWidget):
+    type = "mediaCheck"
     title = _("Check the Integrity of Packages")
     icon = "media-optical-small"
     help = _("""Here you can validate the integrity of the installation packages. A failed validation usually is a sign of a badly mastered installation medium (CD, DVD or USB storage).
 If you are using an optical installation medium, try burning the installation image using DAO (Disc-at-once) mode, at a lower speed (4x for DVD, 8-12x for CD).
 """)
 
-    def __init__(self, *args):
-        QtGui.QWidget.__init__(self,None)
+    def __init__(self):
+        QWidget.__init__(self)
         self.ui = Ui_CheckCDWidget()
         self.ui.setupUi(self)
 
         self.ui.validationSucceedBox.hide()
         self.ui.validationFailBox.hide()
+        self.check_media_stop = True
         self.ui.progressBar.hide()
 
-        self.connect(self.ui.checkButton, SIGNAL("clicked()"),self.slotCheckCD)
+        self.connect(self.ui.checkButton, SIGNAL("clicked()"), self.slotCheckCD)
         if ctx.consts.lang == "tr":
             self.ui.progressBar.setFormat("%%p")
 
     def slotCheckCD(self):
-        if ctx.yali.checkCDStop == True:
-            ctx.yali.checkCDStop = False
+        if self.check_media_stop:
+            self.check_media_stop = False
             self.ui.progressBar.show()
-            icon = QtGui.QIcon()
-            icon.addPixmap(QtGui.QPixmap(":/gui/pics/dialog-error.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+            icon = QIcon()
+            icon.addPixmap(QPixmap(":/gui/pics/dialog-error.png"), QIcon.Normal, QIcon.Off)
             self.ui.checkButton.setIcon(icon)
             self.ui.checkButton.setText("")
             ctx.yali.checkCD(self.ui)
         else:
-            ctx.yali.checkCDStop = True
+            self.check_media_stop = True
             self.ui.progressBar.show()
-            icon = QtGui.QIcon()
-            icon.addPixmap(QtGui.QPixmap(":/gui/pics/task-accepted.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+            icon = QIcon()
+            icon.addPixmap(QPixmap(":/gui/pics/task-accepted.png"), QIcon.Normal, QIcon.Off)
             self.ui.checkButton.setIcon(icon)
             self.ui.checkButton.setText(_("Validate"))
+
+    def checkMedia(self):
+        ctx.mainScreen.disableNext()
+        ctx.mainScreen.disableBack()
+
+        ctx.interface.informationWindow.update(_("Starting validation..."))
+        class PisiUI(pisi.ui.UI):
+            def notify(self, event, **keywords):
+                pass
+            def display_progress(self, operation, percent, info, **keywords):
+                pass
+
+        yali.pisiiface.initialize(ui = PisiUI(), with_comar = False, nodestDir = True)
+        yali.pisiiface.addCdRepo()
+        ctx.mainScreen.processEvents()
+        pkg_names = yali.pisiiface.getAvailablePackages()
+
+        self.ui.progressBar.setMaximum(len(pkg_names))
+
+        cur = 0
+        self.flag = 0
+        for pkg_name in pkg_names:
+            cur += 1
+            ctx.logger.debug("Validating %s " % pkg_name)
+            ctx.interface.informationWindow.update(_("Validating %s") % pkg_name)
+            if self.check_media_stop:
+                continue
+            try:
+                yali.pisiiface.checkPackageHash(pkg_name)
+                self.ui.progressBar.setValue(cur)
+            except:
+                rc  = ctx.interface.messageWindow(_("Warning"),
+                                                  _("Validation of %s package failed."
+                                                    "Please remaster your installation medium and"
+                                                    "reboot.") % pkg_name,
+                                                  type="custom", customIcon="error",
+                                                  customButtons=[_("Skip Validation"), _("Skip Package"), _("Reboot")],
+                                                  default=0)
+                self.flag = 1
+                if not rc:
+                    self.ui.validationBox.hide()
+                    self.ui.validationFailBox.show()
+                    ctx.mainScreen.enableNext()
+                    break
+                elif rc == 1:
+                    continue
+                else:
+                    yali.util.reboot()
+
+
+        if not self.checkCDStop and self.flag == 0:
+            ctx.interface.informationWindow.update(_('<font color="#FFF"><b>Validation succeeded. You can proceed with the installation.</b></font>'))
+            self.ui.validationSucceedBox.show()
+            self.ui.validationBox.hide()
+        else:
+            ctx.interface.informationWindow.hide()
+            self.ui.progressBar.setValue(0)
+
+        yali.pisiiface.removeRepo(ctx.consts.cd_repo_name)
+
+        ctx.mainScreen.enableNext()
+        ctx.mainScreen.enableBack()
+
+        ctx.interface.informationWindow.hide()
+
+register_gui_screen(Widget)
