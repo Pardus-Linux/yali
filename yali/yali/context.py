d3ba12f05e1163b00bb654386910adc90c1158a7
Mete Alpaslan Katırcıoğlu <mete@katircioglu.net>
2010-11-09 19:57:47 +0000

    yali.context:Installation steps type are enum

--- yali/yali/context.py
+++ yali/yali/context.py
@@ -14,6 +14,14 @@ from yali.installdata import installData
 from yali.constants import consts
 from yali.flags import flags
 
+STEP_DEFAULT, STEP_BASE, STEP_OEM_INSTALL, STEP_FIRST_BOOT, STEP_RESCUE = xrange(5)
+
+STEP_TYPE_STRINGS = {STEP_DEFAULT:"Default",
+                     STEP_BASE:"Base System Installation",
+                     STEP_OEM_INSTALL:"OEM Installation",
+                     STEP_FIRST_BOOT:"First Boot mode",
+                     STEP_RESCUE:"System Rescue mode"}
+
 logger = None
 
 storage = None
