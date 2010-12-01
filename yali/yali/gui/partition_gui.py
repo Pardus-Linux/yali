5ea2d842992325f1c8739197be3902fd8ffd6089
Mete Alpaslan Katırcıoğlu <mete@katircioglu.net>
2010-12-01 23:23:22 +0000

    yali.gui.partition_gui:Set filesystem type as default automatically
    If creating physical volume is selected, filesystem type should be
    setted automatically.
    
    BUG:FIXED:15340

--- yali/yali/gui/partition_gui.py
+++ yali/yali/gui/partition_gui.py
@@ -217,8 +217,6 @@ class PartitionWidget(QtGui.QWidget, Ui_PartitionWidget):
             storageGuiHelpers.fillFilesystemMenu(self.filesystemMenu, self.origrequest.format,
                                                  availables=restricts)
             QObject.connect(self.filesystemMenu, SIGNAL("currentIndexChanged(int)"), self.formatTypeChanged)
-            default_format = self.filesystemMenu.findText("ext4")
-            self.filesystemMenu.setCurrentIndex(default_format)
             self.resizeRadio.hide()
             self.resizeSlider.hide()
             self.resizeSpin.hide()
