ab27e373847fb23285a1257ec64622a72e7ec962
Mete Alpaslan Katırcıoğlu <mete@katircioglu.net>
2011-01-20 12:16:26 +0000

    All storage modules are inclueded.

--- yali/tools/update-po.sh
+++ yali/tools/update-po.sh
@@ -4,7 +4,7 @@ LANGUAGES=`ls po/*.po`
 TEMP=`mktemp`
 set -x
 
-xgettext -L "python" --keyword=__tr --keyword=_ --keyword=i18n yali/gui/Ui/*.py yali/gui/*.py yali/storage/*.py yali/*.py -o po/yali.pot
+xgettext -L "python" --keyword=__tr --keyword=_ --keyword=i18n yali/gui/Ui/*.py yali/gui/*.py yali/storage/*.py yali/storage/devices/*.py yali/storage/library/*.py yali/storage/formats/*.py yali/*.py -o po/yali.pot
 for lang in $LANGUAGES
 do
     #msgcat --use-first -o $TEMP $lang po/yali.pot
